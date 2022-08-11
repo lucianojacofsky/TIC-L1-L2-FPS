a580bf6db1ad3addba931926f46eecc445a64c5f
JACOFSKY LUCIANO <47126593@ort>
2022-08-11 08:55:08 -0300

    finish

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject objectToClone;
+
+  
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.N))
+        {
+
+            Instantiate(objectToClone);
+        }
     }
 }
