188b8bdec4a1fc01f0d2b09abafca1c2e6b915aa
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-08-25 08:52:55 -0300

    .

---
+++ Assets/Scripts/BuscarElementoEnArray.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuscarElementoEnArray : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] arrayDeMesas;
+    [SerializeField] string tagDeMesa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrayDeMesas = GameObject.FindGameObjectsWithTag(tagDeMesa);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            FindObjectInArray();
+        }
+        
+        
+    }
+
+    void FindObjectInArray()
+    {
+        bool isFound = false;
+        int i = 0;
+        // para buscar un elemento usamos while
+        while (i<arrayDeMesas.Length && !isFound)
+        {
+            if (arrayDeMesas[i].GetComponent<Mesa>().owner == "Luciano" )
+            {
+                isFound = true;
+                Destroy(arrayDeMesas[i]);
+                return;
+            }
+        }
+    }
+}
